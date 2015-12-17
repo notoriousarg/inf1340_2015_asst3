7c26dc65df0f62ef112991abaff6b7793198f412
ARGreen <adam.rogers.green@gmail.com>
2015-12-16 22:21:20 -0500

    A few spelling changes

--- exercise1.py
+++ exercise1.py
@@ -5,10 +5,8 @@
 This module performs table operations on database tables
 implemented as lists of lists. """
 
-__author__ = 'Susan Sim'
-__email__ = "ses@drsusansim.org"
-__copyright__ = "2015 Susan Sim"
-__license__ = "MIT License"
+__author__ = 'Adam Rogers-Green, Therese Owusu, Paola Santiago'
+
 
 
 #####################
